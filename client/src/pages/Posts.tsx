a4f7fcaea59d9b024d21b7fb9378cbcc990f711d
Insa Sadikov <sadikovinsaf99@gmail.com>
2023-03-06 00:05:30 +0300

    v-1.0.2

--- client/src/pages/Posts.tsx
+++ client/src/pages/Posts.tsx
@@ -1,24 +1,31 @@
 import axios from "axios"
 import "../main.css"
 
-import { useEffect, useState } from "react"
+import { SetStateAction, useEffect, useState } from "react"
 
 
 interface PostType{
-    author:string,
+    author?:string,
     title:string, 
     content:string, 
-    image:string
+    image?:string,
+    id?:string
 }
 
 export const Posts=()=>{
 
     const [posts, setPosts]=useState<PostType[]>()
+    const [value, setValue]=useState<PostType>({ 
+                                                    author:"",
+                                                    title:"", 
+                                                    content:""
+                                                })
     const [error, setError]=useState<unknown|any>()
 
     console.log("RENDERS")
 
     const fetchPosts=async ()=>{
+        
        try{
         const {data} = await axios.get('http://localhost:3000/api/test')
         setPosts(data)
@@ -26,15 +33,45 @@ export const Posts=()=>{
        
        }catch(e){ 
            setError(e)
+           setTimeout(()=>{ 
+            fetchPosts()
+           },6000)
         
        }
-    
-
     }
     
+    const postHandler=async ()=>{ 
+        
+        try{ 
+        localStorage.setItem("author", JSON.stringify(value.author))
+        await axios.post("http://localhost:3000/api/test",value)
+        await fetchPosts()
+
+        
+
+        
+        }catch(e){
+            setError(e)
+        }finally{
+            setValue(prevValue=>{return {...prevValue,title:"", content:""}})
+        }
+
+    }
+
+    const getAuhtor=()=>{
+        
+
+    }
     useEffect(()=>{
         fetchPosts()
+        const localAuthor=localStorage.getItem("author")
+        if(localAuthor){
+            setValue({...value,author:JSON.parse(localAuthor)})
+        }
+        
     },[])
+
+   
     
 
     return <> 
@@ -48,24 +85,45 @@ export const Posts=()=>{
 
                  <div className="post_content">
                     {posts?.map((post)=>{
-                        return <div className="post_item">
+                        return <div key={post.id} className="post_item">
                                    <div className="post_item_top">
-                                        <h2 className="post_item_author">{post.author}</h2>
-                                        <h3 className="post_item_title">{post.title}</h3>
+                                        <h2 className="post_item_author">{post.author}-</h2>
+                                        <h3 className="post_item_title">{post.title}:</h3>
                                    </div>
                                    <div className="post_item_foot">
                                         <p className="post_item_content">
-                                            {post.content}
+                                            "{post.content}"
                                         </p>
                                    </div>
                                     
                                </div>
                     })}
+                     {error&&<p style={{color:"red",textAlign:"center"}}>Something went wrong on server: {error.message}</p>}
                  </div>
                  <div className="post_create">
-
+                    <form className="form">
+                        <input className="post_input" 
+                               placeholder="your name"
+                               value={value.author}
+                               onChange={(e)=>!localStorage.getItem("author")&&setValue({...value,author:e.target.value})}
+                               />
+                        <input className="post_input" 
+                               placeholder="title"
+                               value={value.title}
+                               onChange={(e)=>{setValue({...value,title:(e.target.value)})}}
+                               />
+                        <input className="post_input" 
+                               placeholder="content"
+                               style={{height:"12vh"}}
+                               value={value.content}
+                               onChange={(e)=>{setValue({...value,content:e.target.value})}}
+                               />
+                        <button
+                         onClick={(e)=>{e.preventDefault();postHandler()}} 
+                         className="btn">POST</button>
+                    </form>
                  </div>
-                    
+        
                     
                  </div>
                  
